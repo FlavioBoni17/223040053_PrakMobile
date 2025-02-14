28231a258063fc8d2fd661a10b350b364a6e255b
Flavio Boni <flavioboni17@gmail.com>
2025-02-14 22:45:20 +0700

    Mengerjakan Latihan Pertemuan 1

---
+++ Pertemuan1/TipeDataDanVariabel.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var nama = "Kotlin"
+    println(nama) // Output: Kotlin
+}
+
+// fun main() {
+//     var nama: String = "Kotlin"
+//     println(nama) // Output: Kotlin
+// }
+
+// fun main() {
+//     var umur: Int = 25
+//     println(umur) // Output: 25
+// }
